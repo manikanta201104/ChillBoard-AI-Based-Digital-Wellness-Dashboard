cdf8c3cd2b44f5f60ef0764dd189a4c75af834c4
manikanta201104 <manikanta0224402@gmail.com>
2025-08-10 10:13:19 +0530

    changed

--- Backend/index.js
+++ Backend/index.js
@@ -69,7 +69,7 @@ mongoose
   .then(() => logger.info('Connected to MongoDB'))
   .catch((err) => logger.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
